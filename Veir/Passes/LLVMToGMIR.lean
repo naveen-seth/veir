618f5e4c2c0c57f97653ac2c018cc92ad0428d9e
Naveen Seth Hanig <naveen.hanig@outlook.com>
2026-09-03 16:40:30 +0200

    wip

--- Veir/Passes/LLVMToGMIR.lean
+++ Veir/Passes/LLVMToGMIR.lean
@@ -8,51 +8,43 @@ import Veir.PatternRewriter.Puddle.Execution
 namespace Veir
 
 /-!
-  # LLVMToGMIR pass
-
   This file replicates LLVM's GlobalISel IRTranslator pass.
+  This pass is responsible for translating LLVM IR into gMIR.
 
   The all properties on LLVM-IR operations are carried over into gMIR
-  (similar to upstreams `MachineInstr::copyFlagsFromInstruction`).
-
-  This pass from LLVM IR to gMIR differs from the upstream implementation in the following notable ways:
-  - Upstream converts all poison values to undef values; we only have poison but no undef.
-  - Some informations that we represent as operation properties (e.g. the icmp condition code) are
-    represented as special operands in upstream gMIR.
+  (see upstreams `MachineInstr::copyFlagsFromInstruction`).
 -/
 
 /-! # Lowering Patterns -/
 
 /--
-  gMIR 1:1 lowerings with Puddle for binary operations.
+  gMIR 1:1 lowerings with Puddle for binary integer operations.
 
-  This should mirror ArithToLLVM's lower1to1. To support variable arity, we would need a Puddle equivalent
-  of `pdl.results` and `pdl.operands`.
+  This should mirror ArithToLLVM's lower1to1. To support variable result/operand counts, we would need
+  a Puddle equivalent of `pdl.results` and `pdl.operands`.
 -/
-def lowerBinop (lOp : Llvm) (gOp : GMIR)
+def lowerIntegerBinop (lOp : Llvm) (gOp : GMIR)
     (h : propertiesOf (OpCode.llvm lOp) = propertiesOf (OpCode.gmir gOp) := by rfl) :
     Puddle.Pattern OpCode :=
   Puddle.Pattern.Builder
     (do
-      let lhsType ← Puddle.MatchProg.type (Attr := TypeAttr)
-      let rhsType ← Puddle.MatchProg.type (Attr := TypeAttr)
-      let resultType ← Puddle.MatchProg.type (Attr := TypeAttr)
-      let lhs ← Puddle.MatchProg.value lhsType
-      let rhs ← Puddle.MatchProg.value rhsType
-      let root ← Puddle.MatchProg.root (.llvm lOp) #[lhs, rhs] #[resultType]
-      return (resultType, lhs, rhs, root))
-    (fun (resultType, lhs, rhs, root) => do
+      let returnType ← Puddle.MatchProg.type (Attr := TypeAttr)
+      let lhs ← Puddle.MatchProg.value returnType
+      let rhs ← Puddle.MatchProg.value returnType
+      let root ← Puddle.MatchProg.root (.llvm lOp) #[lhs, rhs] #[returnType]
+      return (returnType, lhs, rhs, root))
+    (fun (returnType, lhs, rhs, root) => do
       let props ← Puddle.CreateProg.applyNative root.properties
                     (fun p => some (cast h p))
-      let newOp ← Puddle.CreateProg.operation (.gmir gOp) #[lhs, rhs] #[resultType] props
+      let newOp ← Puddle.CreateProg.operation (.gmir gOp) #[lhs, rhs] #[returnType] props
       return newOp)
     (fun newOp => newOp)
 
-def g_add_pattern := lowerBinop .add .g_add
+def g_add_pattern := lowerIntegerBinop .add .g_add
 
-def g_sub_pattern := lowerBinop .sub .g_sub
+def g_sub_pattern := lowerIntegerBinop .sub .g_sub
 
-def g_icmp_pattern := lowerBinop .icmp  .g_icmp
+def g_icmp_pattern := lowerIntegerBinop .icmp  .g_icmp
 
 def g_add (rewriter : PatternRewriter OpCode) (op : OperationPtr)
     (opInBounds : op.InBounds rewriter.ctx.raw) : Option (PatternRewriter OpCode) :=
@@ -79,7 +71,7 @@ def LLVMToGMIRPass.impl (ctx : WfIRContext OpCode) (op : OperationPtr)
 
 public def LLVMToGMIRPass : Pass OpCode :=
   { name := "llvm-to-gmir"
-    description := "Lower llvm dialect operations to the gmir dialect."
+    description := "Translate llvm dialect operations to gmir generic instructions."
     run := fun _ => LLVMToGMIRPass.impl }
 
 end Veir
--- Veir/Passes/Legalization.lean
+++ Veir/Passes/Legalization.lean
@@ -1,90 +1,155 @@
 module
 
 public import Veir.Pass
-public import Veir.PatternRewriter.Basic
 import Veir.Passes.Matching
-
-/-!
-  This pass legalizes LLVM operations to prepare for instruction selection.
-  For now, this is restricted to widening integer values.
--/
+import Veir.PatternRewriter.Puddle.Builders
+import Veir.PatternRewriter.Puddle.Execution
+public import Veir.PatternRewriter.Basic
 
 namespace Veir
 
-/--
-  Sigma type for an operation plus its properties.
--/
-abbrev OpWithProp := (op : OpCode) × (propertiesOf op)
+public section
+
+inductive LegalizeAction where
+| legal
+| widenScalar
+| custom
+| unsupported
+| notFound
+deriving Repr, BEq
 
 /--
-  Converts the types of the arguments and result of a single-result binary operation.
+  The LegalityQuery object bundles together all the information that's needed to decide whether a
+  given operation is legal or not.
 -/
-def convertBinaryOp (ctx : WfIRContext OpCode) (op : OperationPtr) (newtype : TypeAttr) (convLhs convRhs newOp convRes : OpWithProp) :
+structure LegalityQuery where
+  opcode : OpCode
+  bitwidths : Array Nat  /- We don't use LTT, just simple bitwidths instead. -/
+
+def queryOf (ctx : WfIRContext OpCode) (op : OperationPtr) : Option LegalityQuery := do
+  let opcode := op.getOpType! ctx.raw
+  let resultTypes := op.getResultTypes! ctx.raw
+  let operandTypes := op.getOperandTypes! ctx.raw
+  let bitwidths ← (resultTypes ++ operandTypes).mapM (Attribute.bitwidthOfType ·.val)
+  some { opcode, bitwidths := bitwidths }
+
+/--
+  The result of a query. It either indicates a final answer of Legal or Unsupported or describes an
+  action that must be taken to make an operation more legal.
+-/
+structure LegalizeActionStep where
+  action : LegalizeAction
+  typeIndex : Nat := 0
+  newBw : Nat := 0
+
+abbrev LegalityPredicate := LegalityQuery → Bool
+abbrev LegalizeMutation := LegalityQuery → Nat × Nat -- returns (typeIndex, newBW)
+
+namespace LegalityPredicates
+
+def sizeInSet (typeIndex : Nat) (sizes : Array Nat) : LegalityPredicate :=
+  fun q => sizes.contains q.bitwidths[typeIndex]!
+
+def sizeNotPow2 (typeIndex : Nat) : LegalityPredicate :=
+  fun q => !Nat.isPowerOfTwo q.bitwidths[typeIndex]!
+
+def scalarNarrowerThan (typeIndex bw : Nat) : LegalityPredicate :=
+  fun q => q.bitwidths[typeIndex]! < bw
+
+end LegalityPredicates
+
+namespace LegalityMutations
+
+def widenScalarToNextPow2 (typeIndex : Nat) : LegalizeMutation :=
+  fun q => (typeIndex, Nat.nextPowerOfTwo q.bitwidths[typeIndex]!)
+
+def clampScalar (typeIndex minBw maxBw : Nat) : LegalizeMutation :=
+  fun q => (typeIndex, min maxBw (max minBw q.bitwidths[typeIndex]!))
+
+end LegalityMutations
+
+/--
+  A single rule in a legalizer info ruleset.
+  The specified action is chosen when the predicate is true. Where appropriate for the action
+  (e.g. for WidenScalar) the new type is selected using the given mutator.
+-/
+structure LegalizeRule where
+  predicate : LegalityPredicate
+  action : LegalizeAction
+  mutation : LegalizeMutation
+
+abbrev LegalizeRuleSet := Array LegalizeRule
+
+private def LegalizeRule.apply (rule : LegalizeRule) (q : LegalityQuery) : Option LegalizeActionStep :=
+  if rule.predicate q then
+    let (typeIndex, newBw) := rule.mutation q
+    some { action := rule.action, typeIndex, newBw }
+  else
+    none
+
+def legalFor (sizes : Array Nat) : LegalizeRule :=
+  { predicate := LegalityPredicates.sizeInSet 0 sizes
+    action := .legal
+    mutation := fun _ => (0, 0) }
+
+def customFor (sizes : Array Nat) : LegalizeRule :=
+  { predicate := LegalityPredicates.sizeInSet 0 sizes
+    action := .custom
+    mutation := fun _ => (0, 0) }
+
+def widenScalarToNextPow2 (typeIndex : Nat) : LegalizeRule :=
+  { predicate := LegalityPredicates.sizeNotPow2 typeIndex
+    action := .widenScalar
+    mutation := LegalityMutations.widenScalarToNextPow2 typeIndex }
+
+def clampScalar (typeIndex minBw maxBw : Nat) : LegalizeRule :=
+  { predicate := LegalityPredicates.scalarNarrowerThan typeIndex maxBw
+    action := .widenScalar
+    mutation := LegalityMutations.clampScalar typeIndex minBw maxBw }
+
+structure LegalizerInfo where
+  ruleSets : Std.HashMap OpCode LegalizeRuleSet := ∅
+  legalizeCustom : LocalRewritePattern OpCode := fun ctx _ => some (ctx, none)
+
+namespace LegalizerInfo
+
+def defineRuleSet
+    (info : LegalizerInfo)
+    (ops : Array OpCode)
+    (rules : LegalizeRuleSet) : LegalizerInfo :=
+  { info with
+    ruleSets := ops.foldl (fun m op => m.insert op rules) info.ruleSets }
+
+def getAction (info : LegalizerInfo) (q : LegalityQuery) : LegalizeActionStep :=
+  match info.ruleSets.get? q.opcode with
+  | none => { action := .notFound }
+  | some ruleset =>
+    (ruleset.findSome? (·.apply q)).getD { action := .notFound }
+
+end LegalizerInfo
+
+end
+
+def widenScalarWPuddle (ctx : WfIRContext OpCode) (op : OperationPtr) (newBw : Nat) :
     Option (WfIRContext OpCode × Option (Array OperationPtr × Array ValuePtr)) := do
-  let type := ((op.getResult 0).get! ctx.raw).type
-  let [lhs, rhs] := (op.getOperands! ctx.raw).toList | return (ctx, none)
-  let (ctx, lhsOp) ← WfRewriter.createOp! ctx convLhs.fst #[newtype] #[lhs] #[] #[] convLhs.snd none
-  let (ctx, rhsOp) ← WfRewriter.createOp! ctx convRhs.fst #[newtype] #[rhs] #[] #[] convRhs.snd none
-  let (ctx, newOp) ← WfRewriter.createOp! ctx newOp.fst #[newtype] #[lhsOp.getResult 0, rhsOp.getResult 0] #[] #[] newOp.snd none
-  let (ctx, resOp) ← WfRewriter.createOp! ctx convRes.fst #[type] #[newOp.getResult 0] #[] #[] convRes.snd none
-  return (ctx, some (#[lhsOp, rhsOp, newOp, resOp], #[resOp.getResult 0]))
+  sorry
 
-/--
-  Integer values can be zero-extended, sign-extended, or any-extended (i.e., extended with arbitrary bits).
--/
-inductive IntegerExtKind
-| any
-| zero
-| sign
-
-/--
-  Return the LLVM operation corresponding to an `IntegerExtKind`.
--/
-def expandIntegerExtOp (type : IntegerExtKind) : ((op : OpCode) × propertiesOf op) :=
-  match type with
-  | .sign => .mk (OpCode.llvm .sext) ()
-  | .zero => .mk (OpCode.llvm .zext) (.mk false)
-  | .any => .mk (OpCode.llvm .zext) (.mk false) -- FIXME
-
-/--
-  Widen the operands and result type of a homogeneously-typed binary LLVM operation.
--/
-def widenSimpleBinaryIntOp (ctx : WfIRContext OpCode) (op : OperationPtr) (newBw : Nat) (extType : IntegerExtKind) (newOp : Option OpWithProp := none) :
+def widenScalar (ctx : WfIRContext OpCode) (op : OperationPtr) (typeGroup : Nat) (newBw : Nat) :
     Option (WfIRContext OpCode × Option (Array OperationPtr × Array ValuePtr)) := do
-  let oldOp := Sigma.mk (op.getOpType! ctx.raw) (op.getProperties! ctx.raw (op.getOpType! ctx.raw))
-  let .integerType ⟨bw⟩ := ((op.getResult 0).get! ctx.raw).type.val | return (ctx, none)
-  if bw ≥ newBw then return (ctx, none)
-  let expandOp := expandIntegerExtOp extType
-  convertBinaryOp ctx op (IntegerType.mk newBw) expandOp expandOp (newOp.getD oldOp) ⟨.llvm .trunc, .mk false false⟩
+  match op.getType! ctx.raw with
+  | .gmir . add =>
+    widenHomogenousBinaryOp ctx op newBw
 
-/--
-  Widen the operands and result type of an LLVM operation.
--/
--- TODO incomplete
-def widenOperations (ctx : WfIRContext OpCode) (op : OperationPtr) :
-    Option (WfIRContext OpCode × Option (Array OperationPtr × Array ValuePtr)) := do
-  match op.getOpType! ctx.raw with
-  | .llvm .add =>
-    widenSimpleBinaryIntOp ctx op 64 .any (some ⟨.llvm .add, .mk false false⟩)
-  | .llvm .sub =>
-    widenSimpleBinaryIntOp ctx op 64 .any (some ⟨.llvm .sub, .mk false false⟩)
-  | .llvm .mul =>
-    widenSimpleBinaryIntOp ctx op 64 .any (some ⟨.llvm .mul, .mk false false⟩)
-  | .llvm .and =>
-    widenSimpleBinaryIntOp ctx op 64 .any (some ⟨.llvm .and, ()⟩)
-  | .llvm .xor =>
-    widenSimpleBinaryIntOp ctx op 64 .any (some ⟨.llvm .xor, ()⟩)
-  | .llvm .or =>
-    widenSimpleBinaryIntOp ctx op 64 .any (some ⟨.llvm .or, .mk false⟩)
-  | _ => return (ctx, none)
+def legalizeInstrStep (info : LegalizerInfo) (ctx : WfIRContext OpCode) (op : OperationPtr) :
+    Option (WfIRContext OpCode × Option (Array OperationPtr × Array ValuePtr)) :=
+  match queryOf ctx op with
+  | none => some (ctx, none)
+  | some q =>
+    let step := info.getAction q
+    match step.action with
+    | .legal => some (ctx, none)
+    | .unsupported | .notFound => some (ctx, none)
+    | .custom => info.legalizeCustom ctx op
+    | .widenScalar => widenScalarWPuddle ctx op step.typeIndex step.newBw
 
-def LegalizePass.impl (ctx : WfIRContext OpCode) (op : OperationPtr) (_ : op.InBounds ctx.raw) :
-    ExceptT String IO (WfIRContext OpCode) := do
-  match RewritePattern.applyInContext (RewritePattern.GreedyRewritePattern #[.fromLocalRewrite widenOperations]) ctx with
-  | none => throw "Error while applying legalization"
-  | some ctx => pure ctx
-
-public def LegalizePass : Pass OpCode :=
-  { name := "legalize"
-    description := "Legalize types."
-    run := fun _ => LegalizePass.impl }
+end Veir
